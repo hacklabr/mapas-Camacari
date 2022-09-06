d0d3404ff8daf51806dd51b6d908a80fbda01818
erleibiazzio <erleibiazzio@gmail.com>
2022-09-06 10:24:12 -0300

    Configura Plugins MapasBlame e LocationPatch

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -8,5 +8,20 @@ return [
         
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+        'MapasBlame' => [
+            'namespace' => 'MapasBlame',
+            'config' => [
+                'request.logData.PATCH' => function ($data) {
+                    return $data;
+                },
+            ]
+        ],
+        "LocationPatch" => [
+            "namespace" => "LocationPatch",
+            "config" => [
+                "enable" => env("LOCATION_PATCH_ENABLE", true),
+                "cutoff" => env("LOCATION_PATCH_CUTOFF", "19800101000001"),
+            ],
+        ],
     ]
 ];
