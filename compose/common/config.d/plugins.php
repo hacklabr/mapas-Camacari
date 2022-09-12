c708519257b3bc846e51a47e392fa80058bd5e38
erleibiazzio <erleibiazzio@gmail.com>
2022-09-12 09:28:15 -0300

    Seta as configurações do plugin SubsiteImporter #3

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -23,5 +23,28 @@ return [
                 "cutoff" => env("LOCATION_PATCH_CUTOFF", "19800101000001"),
             ],
         ],
+        'SubsiteImporter' => [
+            'namespace' => 'SubsiteImporter',
+            'config' => [
+                'public_key' => env('SUBI_PUB_KEY', null),
+                'private_key' => env('SUBI_PRIV_KEY', null),
+                'subsite_importer_password' => "mapas@123",
+                'url_import' => 'http://camacari.ba.mapas.cultura.gov.br/',
+                'query_string' => [
+                    '@files' => '(avatar,gallery,header):url,description',
+                ],
+                'entities_to_import' => ["agent"],
+                'get_metadata' => true,
+                'import_files' => true,
+                'owner_id' => 2,
+                'files_grp_import' => ['avatar', 'header'],
+                'space_cb' => function($space, $entity){},
+                'header_get_userdata' => [
+                    'cookie' => env('SUBI_COOKIE_KEY', null),
+                    'host' => env('SUBI_COOKIE_HOST', null),
+                    'referer' => env('SUBI_COOKIE_REFERER', null),
+                ],
+            ]
+        ]
     ]
 ];
