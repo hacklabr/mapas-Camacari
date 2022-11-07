7e930574ad2405aae4c8f65b1c2fcbe2983ca365
erleibiazzio <erleibiazzio@gmail.com>
2022-11-07 16:21:03 -0300

    Remove orientações desnecessárias

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,9 +5,6 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-echo "EDITE ESTE ARQUIVO E DEFINA AS VARIÁVEIS domain, email e staging"
-exit; # E APAGUE ESSA LINHA
-
 domain=(mapacultural.camacari.ba.gov.br)
 email="nao-responda-secultcamacari@camacari.ba.gov.br" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
