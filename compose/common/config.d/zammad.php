3e6c1c558de71516cca00a6082eb98f904a0b3b8
erleibiazzio <erleibiazzio@gmail.com>
2022-12-22 12:18:00 -0300

    Faz conexões com Zammad e define as configurações iniciais

---
+++ compose/common/config.d/zammad.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'zammad_src_form' => env('ZA_ZAMMAD_SRC_FORM', 'https://camacari.suporte.mapasculturais.com.br/assets/form/form.js'),
+    'zammad_src_chat' => env('ZA_ZAMMAD_SRC_CHAT', 'https://camacari.suporte.mapasculturais.com.br/assets/chat/chat.min.js'),
+    'zammad_enable' => env('ZA_ZAMMAD_ENABLE', true),
+    'zammad_background_color' => env('ZA_ZAMMAD_SRC_CHAT', '#157930'),
+    'zammad_btn_support_enabled' => env('ZA_ZAMMAD_BTN_SUPPORT_ENABLED', true),
+    'zammad_link_support' => env('ZA_ZAMMAD_LINK_SUPPORT', 'email@email.com.br'),
+    'zammad_text_btn_support' => env('ZA_ZAMMAD_TEXT_BTN_SUPPORT', 'Clique aqui'),
+];
--- themes/CAMACARI/layouts/parts/footer.php
+++ themes/CAMACARI/layouts/parts/footer.php
@@ -1,8 +1,8 @@
 </section>
 <footer>
-    
+
     <div id="main-footer">
-       
+
         <div class="row logos">
             <div class="column">
                 <p class="logo-instituicao"><a href="http://secult.camacari.ba.gov.br/"><img src="<?php $this->asset('img/logo-instituicao.png'); ?>" alt="Secult Camaçari/BA"></a></p>
@@ -17,6 +17,13 @@
                 <p class="logo-hacklab">Desenvolvido pelo <a href="https://hacklab.com.br"><img src="<?php $this->asset('img/logo-hacklab.svg'); ?>" alt="Desenvolvido hacklab/"></a></p>
             </div>
         </div>
+        <?php
+        if ($app->config['zammad_btn_support_enabled']) {
+            $linkSipport = $app->config['zammad_link_support'];
+            $textBtnSupport = $app->config['zammad_text_btn_support'];
+            $this->part('support', ["linkSipport" => $linkSipport, "textBtnSupport" => $textBtnSupport]);
+        }
+        ?>
     </div>
 
 </footer>
@@ -24,11 +31,32 @@
 <?php $this->bodyEnd(); ?>
 <iframe id="require-authentication" src="" style="display:none; position:fixed; top:0%; left:0%; width:100%; height:100%; z-index:100000"></iframe>
 
-<?php if ($this->isEditable()): ?>
-    <div id="editbox-human-crop" class="js-editbox" title="<?php \MapasCulturais\i::esc_attr_e("Recortar imagem");?>">
-        <img id="human-crop-image"/>
+<?php if ($this->isEditable()) : ?>
+    <div id="editbox-human-crop" class="js-editbox" title="<?php \MapasCulturais\i::esc_attr_e("Recortar imagem"); ?>">
+        <img id="human-crop-image" />
     </div>
 <?php endif; ?>
 
+<?php if ($app->config['zammad_enable']) : ?>
+    <script src="<?= $app->config['zammad_src_chat']; ?>"></script>
+    <script>
+        $(function() {
+            new ZammadChat({
+                background: '<?= $app->config['zammad_background_color']; ?>',
+                fontSize: '14px',
+                chatId: 1,
+                title: '<strong>Dúvidas?</strong> Fale conosco'
+
+            });
+        });
+    </script>
+    <style>
+        .zammad-chat {
+            z-index: 9999 !important;
+        }
+    </style>
+
+<?php endif ?>
 </body>
+
 </html>
