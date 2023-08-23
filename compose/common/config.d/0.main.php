321c8e6dbded15a08ad4e87801d782c1de060b19
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-08-23 20:52:25 -0300

    configurações

--- compose/common/config.d/0.main.php
+++ compose/common/config.d/0.main.php
@@ -5,15 +5,9 @@ return [
     'app.siteName' => env('SITE_NAME', 'Mapa Cultural de Camaçari'),
     'app.siteDescription' => i::__("O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades"),
 
-    'themes.active' => env('ACTIVE_THEME', 'MapasCulturais\Themes\BaseV1'),
+    'themes.active' => 'CamacariV2',
 
-    'app.lcode' => env('APP_LCODE', 'pt_BR,es_ES'),
-
-    'namespaces' => array(
-        'MapasCulturais\Themes' => THEMES_PATH,
-        'MapasCulturais\Themes\BaseV1' => THEMES_PATH . 'BaseV1/',
-        'Subsite' => THEMES_PATH . 'Subsite/',
-    ),
+    'app.lcode' => 'pt_BR',
 
     'doctrine.database' => [
         'host'           => env('DB_HOST', 'db'),
