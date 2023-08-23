663d1bd0fc287fe42123e2398deeb689465754e3
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-08-23 20:38:19 -0300

    configurações do tema novo

---
+++ compose/common/config.d/maps.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'maps.center'  => [
+        -12.7366164,
+        -38.3058762
+    ],
+    
+    'maps.zoom.default'     => 11,
+    'maps.zoom.approximate' => 14,
+    'maps.zoom.precise'     => 16,
+    'maps.zoom.max'         => 18,
+    'maps.zoom.min'         => 5,
+
+    'maps.tileServer'       => '//{s}.tile.osm.org/{z}/{x}/{y}.png',
+];
---
+++ compose/common/config.d/texts.php
@@ -0,0 +1,23 @@
+<?php
+use \MapasCulturais\i;
+
+return [
+    "text:home-header.title" => i::__("Boas-vindas ao Mapa Cultural de Camaçari!"),
+    "text:home-header.description" => i::__("Essa é uma a plataforma livre, gratuita e colaborativa de mapeamento da Secretaria Municipal de Cultura de Camaçari. Isto quer dizer que o próprio cidadão, agente cultural, seja ele produtor, artista, espaço/instituição cultural ou investidor, é colaborador e responsável pelas informações culturais do nosso município.
+    
+    É muito fácil participar! Basta fazer o seu cadastro e começar a inserir suas informações, as informações de seu grupo ou instituição. Essas informações serão disponibilizadas para todo Brasil, criando oportunidades para nossos artistas. A partir deste cadastro, fica mais fácil participar dos editais de fomento às artes da Prefeitura e também divulgar seus eventos, espaços ou projetos.
+    
+    Os dados que você adiciona neste Mapa são de fundamental importância na construção de um Sistema de Indicadores Culturais de Camaçari, que é uma base importante para definir as políticas públicas para o desenvolvimento da Cultura em nossa cidade.
+    
+    E para você, que é amante da cultura, ficou mais fácil se programar e conhecer as opções culturais que a cidade oferece: shows musicais, espetáculos teatrais e de dança, festivais, manifestações populares, entre outras."),
+
+    "text:home-opportunities.description" => i::__("Cadastre-se, participe de editais e oportunidades e concorra aos benefícios."),
+    "text:home-entities.events" => i::__("Você pode pesquisar eventos culturais nos campos de busca do seu interesse que ajudam na  precisão de sua pesquisa. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente."),
+    "text:home-entities.agents" => i::__("Neste espaço, estão registrados artistas, gestores e produtores; uma rede de atores envolvidos na cena cultural da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas, instituições, empresas, associações, cooperativas e etc)."),
+    "text:home-entities.projects" => i::__("Aqui você encontra leis de fomento, mostras, convocatórias, chamadas públicas e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma."),
+    "text:home-feature.description" => i::__("Confira os últimos destaques de cada uma das áreas."),
+    "text:home-register.title" => i::__("Faça seu cadastro e colabore com o Mapa Cultural de Camaçari"),
+    "text:home-register.description" => i::__("Contribua com a plataforma livre e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas na cidade."),
+    'text:home-developers.description' => 'Além disso, Mapa Cultural de Camaçari é um software livre, criado em parceria entre a hacklab/, secretarias de cultura, organizações não governamentais, empresas e coletivos que investem na plataforma. Você pode contribuir para o seu desenvolvimento através do GitHub.',
+    "text:home-map.description" => i::__("Os agentes, os espaços e os eventos cadastrados contam com a geolocalização de seus endereços, encontre-os aqui:"),
+];
