54f3bc679e74c30ef03103c68978b57064b04a20
erleibiazzio <erleibiazzio@gmail.com>
2023-09-04 15:35:08 -0300

    Altera texto da HOME do ambiente

--- compose/common/config.d/texts.php
+++ compose/common/config.d/texts.php
@@ -3,13 +3,7 @@ use \MapasCulturais\i;
 
 return [
     "text:home-header.title" => i::__("Boas-vindas ao Mapa Cultural de Camaçari!"),
-    "text:home-header.description" => i::__("Essa é uma a plataforma livre, gratuita e colaborativa de mapeamento da Secretaria Municipal de Cultura de Camaçari. Isto quer dizer que o próprio cidadão, agente cultural, seja ele produtor, artista, espaço/instituição cultural ou investidor, é colaborador e responsável pelas informações culturais do nosso município.
-    
-    É muito fácil participar! Basta fazer o seu cadastro e começar a inserir suas informações, as informações de seu grupo ou instituição. Essas informações serão disponibilizadas para todo Brasil, criando oportunidades para nossos artistas. A partir deste cadastro, fica mais fácil participar dos editais de fomento às artes da Prefeitura e também divulgar seus eventos, espaços ou projetos.
-    
-    Os dados que você adiciona neste Mapa são de fundamental importância na construção de um Sistema de Indicadores Culturais de Camaçari, que é uma base importante para definir as políticas públicas para o desenvolvimento da Cultura em nossa cidade.
-    
-    E para você, que é amante da cultura, ficou mais fácil se programar e conhecer as opções culturais que a cidade oferece: shows musicais, espetáculos teatrais e de dança, festivais, manifestações populares, entre outras."),
+    "text:home-header.description" => i::__("O Mapa Cultural de Camaçari é o Sistemas Municipal de Informação e Indicadores Culturais. Desempenha um papel fundamental no planejamento, gestão e monitoramento das políticas culturais do município."),
 
     "text:home-opportunities.description" => i::__("Cadastre-se, participe de editais e oportunidades e concorra aos benefícios."),
     "text:home-entities.events" => i::__("Você pode pesquisar eventos culturais nos campos de busca do seu interesse que ajudam na  precisão de sua pesquisa. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente."),
